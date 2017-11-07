d0ee7860007794d8337159057f37f28d1d82c06d
hbb <875670250@qq.com>
2017-11-07 16:48:45 +0800

    :tomato: 更新版本

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,4 +44,27 @@ export class AppComponent implements OnInit {
   componentList = [];
   title1: string;
   title2: string;
+  refRowData: any;
+  tableData: any[] = [{
+    name: '水爷',
+    date: '2017-08-19',
+    address: '上海市普陀区金沙江路 1518 弄',
+  }, {
+    name: '水爷',
+    date: '2017-08-20',
+    address: '上海市普陀区金沙江路 1518 弄',
+  }, {
+    name: '水爷',
+    date: '2017-08-21',
+    address: '上海市普陀区金沙江路 1518 弄',
+  }, {
+    name: '水爷',
+    date: '2017-08-22',
+    address: '上海市普陀区金沙江路 1510 弄',
+  }];
+
+  handle(ref: any) {
+    console.log(ref.rowData);
+    this.refRowData = ref.rowData;
+  }
 }
